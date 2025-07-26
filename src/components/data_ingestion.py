ed87a23e793eb78fce5f71012561a352586f7947
Ayush <ayushraj2005.muz@gmail.com>
2025-07-26 15:08:09 +0530

    Data pushed to Db

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+import pandas as pd
+load_dotenv()
+
+
+MONGO_URL = os.getenv("MONGO_URL")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+
+client = MongoClient(MONGO_URL)
+db = client[DATABASE_NAME]
+collection = client[COLLECTION_NAME]
+
+files = {
+    "train_data": "data/raw/train.csv",
+    "test_data": "data/raw/test.csv",
+    "rul_data": "data/raw/rul.csv"
+}
+
+for collection_name, file_path in files.items():
+    print(f"📥 Ingesting {file_path} into collection `{collection_name}`...")
+    df = pd.read_csv(file_path)
+    data = df.to_dict(orient="records")
+    db[collection_name].delete_many({})  # Optional: Clear old data
+    db[collection_name].insert_many(data)
+    print(f"✅ Inserted {len(data)} records into `{collection_name}`.")
+
+print("Data ingestion successful")
